93223fbe34c0118c263c131062d3839d85b56aae
monthonrose <monthonspy@hotmail.com>
2016-05-16 01:57:06 +0700

    problem group in MainActivity

--- app/src/main/java/com/fatel/mamtv1/MainActivity.java
+++ app/src/main/java/com/fatel/mamtv1/MainActivity.java
@@ -238,11 +238,12 @@ public class MainActivity extends AppCompatActivity {
                 break;
             case R.id.nav_group_fragment:
                 User currentUser = UserManage.getInstance(this).getCurrentUser();
-                if(currentUser.getIdGroup() == 0)
+                //edit
+                //if(currentUser.getIdGroup() == 0)
                     fragmentClass = GroupFragment.class;
-                else{//มีกลุ่ม
-                    requestGroupInfo(GroupMainActivity.class);
-                }
+//                else{//มีกลุ่ม
+//                    requestGroupInfo(GroupMainActivity.class);
+//                }
                 break;
 
             case R.id.nav_logout_fragment:
